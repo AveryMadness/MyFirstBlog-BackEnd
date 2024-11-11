1e2c4429f6f3425088010bff00bdb7594285d2e2
AveryMadness <averyfd@shaw.ca>
2024-11-11 14:04:10 -0700

    Add POST /posts/ to PostsController

--- MyFirstBlog/Controllers/PostsController.cs
+++ MyFirstBlog/Controllers/PostsController.cs
@@ -1,3 +1,6 @@
+using System.Security.Cryptography;
+using MyFirstBlog.Entities;
+
 namespace MyFirstBlog.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
@@ -30,5 +33,56 @@ public class PostsController : ControllerBase {
         }
 
         return post;
+    }
+
+    [HttpPost]
+    public ActionResult NewPost(NewPost newPost)
+    {
+        List<string> errors = new List<string>();
+        if (newPost != null)
+        {
+            if (string.IsNullOrEmpty(newPost.Title))
+            {
+                errors.Add("Title cannot be empty.");
+            }
+
+            if (string.IsNullOrEmpty(newPost.Description))
+            {
+                errors.Add("Description cannot be empty.");
+            }
+        }
+        else
+        {
+            errors.Add("Post is invalid.");
+        }
+
+       
+
+        string Slug = newPost.Title.ToLower().Trim().Replace(" ", "-");
+
+        if (_postService.GetPost(Slug) != null)
+        {
+            errors.Add($"Post with title '{newPost.Title}' already exists.");
+        }
+        
+        Guid NewPostGuid = Guid.NewGuid();
+
+        Post post = new()
+        {
+            Body = newPost.Description,
+            CreatedDate = DateTime.UtcNow,
+            Id = NewPostGuid,
+            Slug = Slug,
+            Title = newPost.Title
+        };
+        
+        if (errors.Count > 0)
+        {
+            return BadRequest(new {errors});
+        }
+
+        _postService.CreatePost(post);
+        
+        return Ok(new {post});
     }
 }
